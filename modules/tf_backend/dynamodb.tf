d9149a59201ccb97d55a15e1b0e153a0dfdf5129
gomi_ningen <gomi2ngen@gmail.com>
2018-03-02 21:48:42 +0900

    Fix read/write capacities of backend DynamoDB

--- modules/tf_backend/dynamodb.tf
+++ modules/tf_backend/dynamodb.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "backend_dynamodb_table" {
   name           = "${var.backend_bucket_name}_terraform_tfstate_lock"
-  read_capacity  = 20
-  write_capacity = 20
+  read_capacity  = 1
+  write_capacity = 1
   hash_key       = "LockID"
 
   attribute {
